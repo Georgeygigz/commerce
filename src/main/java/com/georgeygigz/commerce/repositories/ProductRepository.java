83b9ffb1333aefe49ddb440e95cf4567e3a12e7b
georgeygigz <georgeymutti@gmail.com>
2025-07-03 14:29:48 +0300

    product endpoints

--- src/main/java/com/georgeygigz/commerce/repositories/ProductRepository.java
+++ src/main/java/com/georgeygigz/commerce/repositories/ProductRepository.java
@@ -1,6 +1,7 @@
 package com.georgeygigz.commerce.repositories;
 
 import com.georgeygigz.commerce.entities.Product;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
@@ -10,8 +11,13 @@ import java.util.List;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
 
-    @Query("SELECT p FROM Product p WHERE :categoryId = 0 OR p.category.id = :categoryId")
-    List<Product> findByCategoryId(@Param("categoryId") @RequestParam(required = false, defaultValue = "0") int categoryId);
+    @EntityGraph(attributePaths = "category")
+    List<Product> findByCategoryId(Byte categoryId);
+
+    @EntityGraph(attributePaths = "category")
+    @Query("SELECT p FROM Product p ")
+    List<Product> findAllWithCategory();
+
 
 
 }
